77fda19e66dfa3c2a93e0a837b1020ec89cac0e2
SimyV <s.lobascher@gmail.com>
2022-11-14 08:35:36 +1100

    commit-splash

--- assets/js/main.js
+++ assets/js/main.js
@@ -287,3 +287,39 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+/**
+   * Splash Animation
+   */
+  let intro = document.querySelector('.intro');
+  let logo = document.querySelector('.logo-header');
+  let logoSpan = document.querySelectorAll('.splash-logo');
+
+  window.addEventListener('DOMContentLoaded', ()=> {
+
+    setTimeout(()=>{
+
+      logoSpan.forEach((span, idx)=> {
+        setTimeout(()=>{
+          span.classList.add('active');
+
+        }, (idx + 1) * 600)
+      });
+
+      setTimeout(()=>{
+        logoSpan.forEach((span,idx)=>{
+
+          setTimeout(()=>{
+            span.classList.remove('active');
+            span.classList.add('fade');
+
+          }, (idx + 1) * 100)
+        })
+      }, 3000)
+
+      setTimeout(()=>{
+        intro.style.top = '-100vh';
+      }, 3300)
+    })
+
+  })
